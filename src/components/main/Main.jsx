4c9d3111ffac5fb199ad99aab493aaffe4fe827c
gmurad97 <murad.dev@bk.ru>
2024-05-05 03:04:44 +0400

    fixing

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,7 +5,7 @@ import Slider from "./components/slider/Slider";
 import Breadcrumb from "./components/breadcrumb/Breadcrumb";
 import Card from "./components/card/Card";
 
-import product_list from "../../mock_data/products.js"; // PRODUCT LIST - MOCK DATA
+import product_list from "../../mock_data/products.js";
 
 const Main = () => {
     return (
@@ -25,13 +25,13 @@ const Main = () => {
                     <h1 className="article__title">Products</h1>
                     <div className="card__wrapper">
                         {product_list.map((product) => (
-                            <Card 
-                            key={product.id}
-                            product_id={product.id}
-                            product_title={product.title}
-                            product_description={product.description}
-                            product_price={product.price}
-                            product_img_name={product.img_name} 
+                            <Card
+                                key={product.id}
+                                product_id={product.id}
+                                product_title={product.title}
+                                product_description={product.description}
+                                product_price={product.price}
+                                product_img_name={product.img_name}
                             />
                         ))}
                     </div>
